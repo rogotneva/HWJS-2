c1d339dd690fd005a463417ee3e49e8e147963e1
rogotneva <pr_rogotneva@mail.ru>
2022-08-12 10:52:56 +0300

    Внесены правки в домашнее задание

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,30 +1,17 @@
-//Задание 1
-// const menuLinks = Array.from(document.querySelectorAll('.menu__link'));
-// menuLinks.forEach(function(link) {
-//   link.onclick = function(){
-//     const subMenu = link.nextElementSibling;
-//
-//     if (subMenu !== null) {
-//       subMenu.classList.toggle('menu_active');
-//       return false;
-//     }
-//   }
-// });
-
 //Задание 1#
 const menuLinks = Array.from(document.querySelectorAll('.menu__link'));
 const hasSubMenu = menuLinks.filter(link => link.nextElementSibling !== null);
-// let lastClick; //При клике на одно подменю, закрывается уже открытое, как и должно быть.
-    // Но не закрывается то же подменю при повторном клике.
 
 hasSubMenu.forEach(function(link) {
   link.onclick = function() {
-    let lastClick; //Открывается и закрывается при клике на одну кнопку с подменю;
-    if (lastClick !== undefined) {
-      lastClick.nextElementSibling.classList.toggle('menu_active');
-    }
-    link.nextElementSibling.classList.toggle('menu_active');
-    lastClick = link;
+    let clickLink = link;
+    hasSubMenu.forEach(function(link) {
+      if (link === clickLink) {
+        link.nextElementSibling.classList.toggle('menu_active');
+      } else {
+        link.nextElementSibling.classList.remove('menu_active');
+      }
+    })
     return false;
   }
 })
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,52 +1,40 @@
 const rightArrow = document.querySelector('.slider__arrow_next');
 const leftArrow = document.querySelector('.slider__arrow_prev');
 const itemsArr = Array.from(document.getElementsByClassName('slider__item'));
-let activeItem = document.querySelector('.slider__item_active');
+const dotsArr = Array.from(document.getElementsByClassName('slider__dot'));
 
-let activeItemIndex = 0;
+let activeItemIndex = itemsArr.findIndex((link) => link.className = 'slider__item slider__item_active');
 
 rightArrow.onclick = showNextItem;
 leftArrow.onclick = showPreviousItem;
 
 function showNextItem() {
-  activeItemIndex++;
-
-  if (activeItemIndex === itemsArr.length) {
-    itemsArr[activeItemIndex - 1].className = 'slider__item';
+  itemsArr[activeItemIndex].className = 'slider__item';
+  if (activeItemIndex === itemsArr.length - 1) {
     activeItemIndex = 0;
     itemsArr[activeItemIndex].className = 'slider__item slider__item_active';
   } else {
-    itemsArr[activeItemIndex - 1].className = 'slider__item';
+    activeItemIndex++;
     itemsArr[activeItemIndex].className = 'slider__item slider__item_active';
   }
 }
 
 function showPreviousItem() {
-  activeItemIndex--;
-
-  if (activeItemIndex < 0) {
-    itemsArr[activeItemIndex + 1].className = 'slider__item';
+  itemsArr[activeItemIndex].className = 'slider__item';
+  if (activeItemIndex === 0) {
     activeItemIndex = itemsArr.length - 1;
     itemsArr[activeItemIndex].className = 'slider__item slider__item_active';
   } else {
-    itemsArr[activeItemIndex + 1].className = 'slider__item';
+    activeItemIndex--;
     itemsArr[activeItemIndex].className = 'slider__item slider__item_active';
   }
 }
 
 //Задание 1#
-const dotsArr = Array.from(document.getElementsByClassName('slider__dot'));
-let lastItem = null;
-
 dotsArr.forEach(function(dot, index) {
   dot.onclick = function() {
+    itemsArr[activeItemIndex].className = 'slider__item';
     activeItemIndex = index;
-
-    if (lastItem === null) lastItem = activeItemIndex;
-
-    itemsArr[lastItem].className = 'slider__item';
     itemsArr[activeItemIndex].className = 'slider__item slider__item_active';
-
-    return lastItem = activeItemIndex;
   }
 });
