6d2041d54a5a5f784b011b2588c1762a9167c83b
rogotneva <pr_rogotneva@mail.ru>
2022-08-17 14:44:58 +0300

    Внесены правки в домашнее задание

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -5,13 +5,38 @@ const book = document.querySelector('.book');
 fontSizeArr.forEach((element) => element.addEventListener('click', (event) => {
   fontSizeArr.forEach((element) => element.classList.remove('font-size_active'));
   event.target.classList.add('font-size_active');
-  const {size} = element.dataset;
-  if (size === 'small') {
-    book.classList.add('book_fs-small');
-  } else if (size === 'big') {
-    book.classList.add('book_fs-big');
-  } else {
-    book.classList.remove('book_fs-small');
-    book.classList.remove('book_fs-big');
-  }
+
+  const {size} = event.target.dataset;
+  book.classList.remove('book_fs-big', 'book_fs-small');
+  book.classList.add('book_fs-' + size);
+
+  event.preventDefault();
+}));
+
+//Задание 1#
+const colorArr = Array.from(document.getElementsByClassName('color'));
+const fontControl = colorArr.filter((element) => element.closest('.book__control_color'));
+const backgroundControl = colorArr.filter((element) => element.closest('.book__control_background'));
+
+fontControl.forEach((element) => element.addEventListener('click', (event) => {
+  fontControl.forEach((element) => element.classList.remove('color_active'));
+  event.target.classList.add('color_active');
+
+  const {textColor} = event.target.dataset;
+  book.classList.remove('book_color-black', 'book_color-gray', 'book_color-whitesmoke');
+  book.classList.add('book_color-' + textColor);
+
+  event.preventDefault();
+}));
+
+
+backgroundControl.forEach((element) => element.addEventListener('click', (event) => {
+  backgroundControl.forEach((element) => element.classList.remove('color_active'));
+  event.target.classList.add('color_active');
+
+  const {bgColor} = event.target.dataset;
+  book.classList.remove('book_bg-black', 'book_bg-gray', 'book_bg-white');
+  book.classList.add('book_bg-' + bgColor);
+
+  event.preventDefault();
 }));
