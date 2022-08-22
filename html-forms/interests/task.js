e1e7dad2d317343df8fe0da4aed4ec555cc5a9d9
rogotneva <pr_rogotneva@mail.ru>
2022-08-22 15:22:35 +0300

    Исправлен баг чекбокса

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,8 +1,17 @@
 const interestsList = Array.from(document.querySelectorAll('.interests_active'));
 const mainInterests = interestsList.map((int) => int.closest('li').firstElementChild.firstElementChild);
+const subInterests = mainInterests.map((int) => int.closest('li.interest').lastElementChild.querySelectorAll('input'));
 
 mainInterests.forEach((int) => int.addEventListener('change', (e) => {
-  let subInt = e.target.closest('li.interest').lastElementChild.querySelectorAll('input');
-  subInt = Array.from(subInt);
-  subInt.forEach((input) => input.checked === true ? input.checked = false : input.checked = true);
+  let subInt = Array.from(e.target.closest('li.interest').lastElementChild.querySelectorAll('input'));
+  subInt.forEach((input) => input.checked = e.target.checked);
 }));
+
+subInterests.forEach((intGroup) => {
+  intGroup.forEach((int) => int.addEventListener('change', (e) => {
+    let mainInt = int.closest('ul.interests').previousElementSibling.querySelector('input');
+
+    Array.from(intGroup).every((int) => int.checked === true) ?
+    mainInt.checked = true : mainInt.checked = false;
+  }));
+});
