7b9560fcef41b4935a7b30fe278bb4fdb5bebe68
rogotneva <pr_rogotneva@mail.ru>
2022-08-24 19:29:57 +0300

    Внесены правки

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,18 +4,30 @@ linksArr.forEach((link) => {
   const tooltip = document.createElement('div');
   tooltip.classList.add('tooltip');
   tooltip.textContent = link.title;
+  link.insertAdjacentElement('afterEnd', tooltip);
 
   link.addEventListener('click', (e) => {
-    let position = link.getBoundingClientRect();
-    tooltip.setAttribute('style', `left: ${position.left}px; top: ${position.top + 20}px;`);
-    link.insertAdjacentElement('afterEnd', tooltip);
-
-    if (document.querySelector('.tooltip_active')) {
-      document.querySelector('.tooltip_active').classList.remove('tooltip_active');
+    const tooltipActive = document.querySelector('.tooltip_active');
+    if (tooltipActive) {
+      if (tooltipActive.textContent === link.title) {
+        tooltipActive.classList.remove('tooltip_active')
+      } else {
+        tooltipActive.classList.remove('tooltip_active');
+        tooltip.classList.add('tooltip_active');
+      }
+    } else {
+      tooltip.classList.add('tooltip_active');
     }
 
-    tooltip.classList.toggle('tooltip_active');
+    let position = link.getBoundingClientRect();
+    tooltip.setAttribute('style', `left: ${position.left}px; top: ${position.top + 20}px;`);
 
     e.preventDefault();
   });
+
+  window.addEventListener('scroll', (e) => {
+    if (document.querySelector('.tooltip_active')) {
+    document.querySelector('.tooltip_active').classList.remove('tooltip_active');
+    }
+  })
 });
